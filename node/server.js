d15bd769b7a57e4840b92abe05f91b5b680f4840
Sherry Jin <sjin@thoughtworks.com>
2012-11-08 12:42:49 -0800

    changes to node, rails blog initial commit

--- node/server.js
+++ node/server.js
@@ -1,11 +1,16 @@
 var http = require("http");
+var url = require("url");
 
+function start(route, handle) {
 function onRequest(request, response) {
-  console.log("Request received.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+  var pathName = url.parse(request.url).pathname;
+  console.log("Request for " + pathName + " received.");
+    
+  route(handle, pathName, response);
 }
 
 http.createServer(onRequest).listen(8888);
 console.log("Server has started.");
+}
+
+exports.start = start;
---
+++ rails/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6b5c6b85f18f4dbb8666fd4e8bfa30ff32ccfc257580f128c32f8e1e05820bdfd95720dceed4e8814164fb72971dca372504409040664626a8625f2e106450c5'
