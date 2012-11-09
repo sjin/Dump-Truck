ef08177bbf125fdf12306870faa0c74a9e7533bf
Sherry Jin <sjin@thoughtworks.com>
2012-11-09 09:36:47 -0800

    home routes to posts/index, comments link

--- rails/blog/config/routes.rb
+++ rails/blog/config/routes.rb
@@ -3,7 +3,7 @@ Blog::Application.routes.draw do
   resources :posts
   resources :comments, :only =>[:new, :create, :destroy]
 
-  root :to => 'home#index'
+  root :to => 'posts#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
